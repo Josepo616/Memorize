51cbc947b19b6199cbc4ef4e325d911099ceedac
José Álvarez <josemejia61976751@gmail.com>
2025-07-15 09:29:34 -0600

    initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by JoseAlvarez on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
