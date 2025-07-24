8b7c613a3a736cf405754c119eff304a33b62f81
José Álvarez <josemejia61976751@gmail.com>
2025-07-24 10:30:54 -0600

    feat: model created and functional

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -6,29 +6,118 @@
 //
 
 import Foundation
-
-struct MemoryGame<CardContent>{
+//Main struct for our game and changes on it
+struct MemoryGame<CardContent> where CardContent: Equatable{
+    //initialization of the game, empty at the beginning; actual game should be created using the button in the UI
     private(set) var cards: Array<Card>
-    
+    //Var for score
+    var newScore = 0
     init(numberOfPairOfCards: Int, cardContentFactory: (Int) -> CardContent){
         cards = []
-        for pairIndex in 0..<max(2, numberOfPairOfCards){
-            let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+        if(numberOfPairOfCards > 0){
+            for pairIndex in 0..<max(2, numberOfPairOfCards){
+                let content = cardContentFactory(pairIndex)
+                cards.append(Card(content: content, id: "\(pairIndex+1)a"))
+                cards.append(Card(content: content, id: "\(pairIndex+1)b"))
+            }
         }
     }
-    
-    func shuffle() {
-        
+    //Get the index of the current face up card
+    var indexOfTheOnlyFaceUpCard: Int?{
+        get { return cards.indices.filter{index in cards[index].isFaceUp}.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) } }
     }
-    func choose(_ card: Card){
-        
+    //Shuffle the array of cards
+    mutating func shuffle() {
+        cards.shuffle()
     }
-    
-    struct Card{
-        var isFaceUp = true
+    //Compare the first two face up cards, if they are equal mark the bools true and update the score
+    mutating func choose(_ card: Card){
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }){
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched{
+                if let potentialMatchIndex = indexOfTheOnlyFaceUpCard{
+                    if cards[potentialMatchIndex].content == cards[chosenIndex].content{
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialMatchIndex].isMatched = true
+                    }
+                    updateEarnScore(newScore: newScore, isMatched: cards[potentialMatchIndex].isMatched)
+                } else {
+                    indexOfTheOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
+    }
+    //Func for updating the score
+    private mutating func updateEarnScore(newScore: Int, isMatched: Bool) {
+        self.newScore += (isMatched ? 2 : -1)
+    }
+    //Main struct for cards,
+    struct Card: Equatable, Identifiable{
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
+        var id: String
+    }
+}
+//We create a enum data type for the array of emojis, the theme of the cards, descriptions/label Title and the number of pairs
+enum Theme {
+    case halloween
+    case cars
+    case animals
+    case sports
+    case flags
+    case food
+    case noone
+    var colorTheme: String{
+     switch self {
+        case .halloween: return "halloween"
+        case .cars:  return "gray"
+        case .animals: return "animal"
+        case .sports: return "red"
+        case .flags: return "blue"
+        case .food: return "yellow"
+        case .noone: return "black"
+        }
+    }
+    var description: String {
+        switch self {
+        case .halloween: return "Your're playing: Halloween theme"
+        case .cars: return "Your're playing: Cars theme"
+        case .animals: return "You're playing: Animals theme"
+        case .sports: return "You're playing: Sports theme"
+        case .flags: return "You're playing: Flags theme"
+        case .food: return "You're playing: Food theme"
+        case .noone: return "You're not playing a theme"
+        }
+    }
+    var emojiElements: [String] {
+        var emojiElementsShuffled: [String]
+        switch self {
+        case .halloween: emojiElementsShuffled = ["🎃", "👻", "🧛‍♂️", "👽", "🧟‍♀️"]
+        case .cars: emojiElementsShuffled =  ["🚗", "🚙" , "🚚", "🚛", "🚜", "🏎️", "🚔"]
+        case .animals: emojiElementsShuffled =  ["🐈", "🐫", "🐰", "🐇", "🐹", "🐻", "🐼", "🐨"]
+        case .sports: emojiElementsShuffled = ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎱", "🏓", "⛳️", "🏆", "🤼"]
+        case .flags: emojiElementsShuffled = ["🇦🇨", "🇦🇴", "🇦🇷", "🇦🇺", "🇧🇪", "🇨🇭", "🇭🇳", "🇩🇪", "🇸🇿", "🇪🇺", "🇬🇪", "🇷🇺"]
+        case .food: emojiElementsShuffled = ["🍗", "🥐", "🍞", "🥖", "🫓", "🥨", "🥯", "🥞", "🧇", "🧀", "🍖", "🍗"]
+        case .noone: emojiElementsShuffled =  []
+        }
+        return emojiElementsShuffled.shuffled()
+    }
+    var numberOfPair: Int {
+        switch self {
+        case .halloween: return Int.random(in: 1...emojiElements.count)
+        case .cars: return Int.random(in: 1...emojiElements.count)
+        case .animals: return Int.random(in: 1...emojiElements.count)
+        case .sports: return Int.random(in: 1...emojiElements.count)
+        case .flags: return Int.random(in: 1...emojiElements.count)
+        case .food: return Int.random(in: 1...emojiElements.count)
+        case .noone: return 0
+        }
+    }
+}
+extension Array{
+    var only: Element? {
+         count == 1 ? first : nil
     }
 }
